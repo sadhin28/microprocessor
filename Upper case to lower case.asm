8dd297d3197236d63b5edf555a0e54c1d7dc6f73
sadhin28 <taosifsadhin113@gmail.com>
2025-05-27 08:41:36 +0600

    add all

--- Upper case to lower case.asm
+++ Upper case to lower case.asm
@@ -1,50 +1,42 @@
-.model small 
-.stack 100h
-.data
-entercharmsg db 'Enter a Character: $' 
-changeChar db 'Uppercse is: $'
-.code
-main:
-mov ax, @data   
-mov ds, ax
-          
-lea dx, entercharmsg  ;for display msg
-mov ah, 9
+
+.model small
+.data 
+     msg1 db 13,10, "Enter an upper case letter: $"
+     msg2 db 13,10, "In lower case: $"
+.code 
+main proc
+    
+    mov ax,@data
+    mov dx,ax
+    
+    mov dx,offset msg1
+    
+    mov ah,9
+    int 21h
+
+mov ah,1
 int 21h 
-           
-           
-mov ah, 1   ;for input char
+
+mov bl,al
+
+add bl,32 ;for lower case to upper case simply sub bl,32
+
+ mov ax,@data
+    mov dx,ax
+    
+    mov dx,offset msg2
+    
+    mov ah,9
+    int 21h
+
+mov dl,bl
+
+mov ah,2
 int 21h
 
-
-
-cmp al, 'a'
-jb not_lower
-cmp al, 'z'
-ja not_lower
-and al, 11011111b 
-
-
-jmp print_result
-
-
-not_lower:
- cmp al, 'A'
- jb end_prog
- cmp al, 'Z'
- ja end_prog
- or al, 00100000b
- 
- print_result:  
-    mov ah, 9
-    lea dx, changeChar  ;for display msg
-
-    int 21h
-    mov dl, al
-    mov ah, 2h
-    int 21h
+mov ah,4ch
+int 21h
     
- end_prog:
-    mov ah, 4ch
-    int 21h
- end main
+    
+  main endp
+end main
